4af24278c5b073cfde56f2f4b7c5c961f64d2c2c
nightwolfz <nightwolfz@gmail.com>
2013-11-21 19:33:40 +0100

    Fix some project settings

--- MvcDating/Controllers/WinkController.cs
+++ MvcDating/Controllers/WinkController.cs
@@ -1,13 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.Entity;
-using System.Linq;
-using System.Net;
-using System.Web;
-using System.Web.Mvc;
-using AutoMapper;
-using Google.GData.Contacts;
+﻿using System.Web.Mvc;
 using MvcDating.Models;
 using MvcDating.Services;
 using WebMatrix.WebData;
